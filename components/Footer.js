94ee927a6aa338e8f40e23ffe80eb42e6ea6b2b7
Nanak <iamnanak01@gmail.com>
2024-08-30 02:28:58 +0530

    fix some css

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer class="text-gray-600 body-font">
-  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <hr/>
+      <footer class="text-gray-600 body-font flex justify-center">
+  <div class="px-5 py-8 mx-auto flex w-full items-center sm:flex-row flex-col">
     <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
